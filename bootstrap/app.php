926942659eb139b0813aec28258745f031cefa9b
Jeremy ROBERT <contact@jeremyrobert.fr>
2024-03-25 13:54:26 +0100

    Delete unnecessary code

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,10 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        // web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api/v1.php',
-        // commands: __DIR__.'/../routes/console.php',
-        health: '/up',
         apiPrefix: 'api/v1',
         then: function () {
             Route::prefix('api/v2')->group(function () {
@@ -22,11 +19,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
-
-        $middleware->alias([
-            'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
-        ]);
-
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
