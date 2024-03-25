5cbdd9f2b585e7de0735de6c158ec99ade0b8109
Jeremy ROBERT <contact@jeremyrobert.fr>
2024-03-25 11:25:45 +0100

    Init Administrator model

---
+++ app/Models/Administrator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Administrator extends Authenticatable
+{
+    use HasApiTokens, HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+    ];
+}
