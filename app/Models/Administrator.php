94d3aebf72ab4fd9f29616f82bf62b6ed759231f
Jeremy ROBERT <contact@jeremyrobert.fr>
2024-03-25 17:37:31 +0100

    Add password field to Administrator

--- app/Models/Administrator.php
+++ app/Models/Administrator.php
@@ -16,7 +16,28 @@ class Administrator extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
         'email',
+        'password',
     ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
 }
