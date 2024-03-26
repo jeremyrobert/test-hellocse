e434c605c562c49f9438c4ce12ec5da73834dbfb
Jeremy ROBERT <contact@jeremyrobert.fr>
2024-03-26 10:32:39 +0100

    Refactor authentication routes

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -3,14 +3,12 @@
 use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('auth/register', [AuthController::class, 'register'])
-    ->middleware('guest')
-    ->name('register');
+Route::middleware('guest')->group(function () {
+    Route::post('auth/register', [AuthController::class, 'register'])->name('register');
+    Route::post('auth/login', [AuthController::class, 'login'])->name('login');
+});
 
-Route::post('auth/login', [AuthController::class, 'login'])
-    ->middleware('guest')
-    ->name('login');
-
-Route::post('auth/logout', [AuthController::class, 'logout'])
-    ->middleware('auth:sanctum')
-    ->name('logout');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('auth/refresh-token', [AuthController::class, 'refreshToken'])->name('refresh-token');
+    Route::post('auth/logout', [AuthController::class, 'logout'])->name('logout');
+});
