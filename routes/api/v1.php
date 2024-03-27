eaad657e1a7a974b86e717575f5862bafb390e6b
Jeremy ROBERT <contact@jeremyrobert.fr>
2024-03-27 16:23:06 +0100

    Add POST /profile endpoint

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,11 +1,8 @@
 <?php
 
-use App\Enums\TokenAbility;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum', 'ability:'.TokenAbility::ACCESS_API->value)->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('profile', [ProfileController::class, 'store']);
 });
