434d71d62fafc3b0cbab54ec972f6ca62776f89f
Jeremy ROBERT <contact@jeremyrobert.fr>
2024-03-27 22:37:45 +0100

    Add profile update test

--- tests/Feature/Profile/ProfileTest.php
+++ tests/Feature/Profile/ProfileTest.php
@@ -13,11 +13,11 @@ class ProfileTest extends TestCase
     use RefreshDatabase;
 
     /**
-     * Test a guest can't store a profile.
+     * Test a guest can not store a profile.
      */
-    public function test_guest_cant_store_profile()
+    public function test_guest_can_not_store_profile()
     {
-        $response = $this->postJson(route('api.profile.store'));
+        $response = $this->postJson(route('api.profiles.store'));
 
         $response->assertUnauthorized();
     }
@@ -27,13 +27,15 @@ class ProfileTest extends TestCase
      */
     public function test_administrator_can_store_profile()
     {
+        Storage::fake('local');
+
         $administrator = Administrator::factory()->create();
 
         $response = $this->actingAs($administrator)
-            ->postJson(route('api.profile.store'), [
+            ->postJson(route('api.profiles.store'), [
                 'first_name' => 'John',
                 'last_name' => 'Doe',
-                'image' => UploadedFile::fake()->image('avatar.jpg')->size(100),
+                'image' => UploadedFile::fake()->image('image.jpg')->size(100),
                 'status' => 'active',
             ]);
 
@@ -48,18 +50,18 @@ class ProfileTest extends TestCase
             'created_at',
         ]);
 
-        Storage::disk('public')->assertExists($response['image']);
+        Storage::disk('local')->assertExists('public/images/'.$response['image']);
     }
 
     /**
-     * Test an administrator can't store a profile with invalid data.
+     * Test an administrator can not store a profile with invalid data.
      */
-    public function test_administrator_cant_store_profile_with_invalid_data()
+    public function test_administrator_can_not_store_profile_with_invalid_data()
     {
         $administrator = Administrator::factory()->create();
 
         $response = $this->actingAs($administrator)
-            ->postJson(route('api.profile.store'), [
+            ->postJson(route('api.profiles.store'), [
                 'first_name' => '',
                 'last_name' => '',
                 'image' => 'invalid-image',
@@ -73,5 +75,44 @@ class ProfileTest extends TestCase
             'image',
             'status',
         ]);
+    }
+
+    /**
+     * Test an administrator can update a profile.
+     */
+    public function test_administrator_can_update_profile()
+    {
+        Storage::fake('local');
+
+        $administrator = Administrator::factory()->create();
+
+        $profile = $administrator->profiles()->create([
+            'last_name' => 'Doe',
+            'first_name' => 'John',
+            'image' => UploadedFile::fake()->image('image.jpg')->size(100),
+            'status' => 'active',
+        ]);
+
+        $response = $this->actingAs($administrator)
+            ->putJson(route('api.profiles.update', $profile), [
+                'first_name' => 'Jane',
+                'last_name' => 'Smith',
+                'image' => UploadedFile::fake()->image('image.jpg')->size(100),
+                'status' => 'inactive',
+            ]);
+
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'id',
+            'administrator_id',
+            'last_name',
+            'first_name',
+            'image',
+            'status',
+            'created_at',
+        ]);
+
+        Storage::disk('local')->assertMissing('public/images/'.$profile->image);
+        Storage::disk('local')->assertExists('public/images/'.$response['image']);
     }
 }
