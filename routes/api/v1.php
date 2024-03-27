dcefa00321322a89eb62a28620f8e20c9c8c3f58
Jeremy ROBERT <contact@jeremyrobert.fr>
2024-03-27 22:47:40 +0100

    Add delete route for profiles

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->name('api.profiles.')->group(function () {
     Route::post('profiles', [ProfileController::class, 'store'])->name('store');
     Route::match(['put', 'patch', 'post'], 'profiles/{profile}', [ProfileController::class, 'update'])->where('profile', '[0-9]+')->name('update');
+    Route::delete('profiles/{profile}', [ProfileController::class, 'destroy'])->where('profile', '[0-9]+')->name('destroy');
 });
