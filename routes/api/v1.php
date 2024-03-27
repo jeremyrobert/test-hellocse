61aae328e3e01b2236c133958ae6be1dedad3ae8
Jeremy ROBERT <contact@jeremyrobert.fr>
2024-03-27 16:44:44 +0100

    Refactor route names in auth and profile

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -4,5 +4,7 @@ use App\Http\Controllers\Api\V1\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('profile', [ProfileController::class, 'store']);
+    Route::name('api.profile.')->group(function () {
+        Route::post('profile', [ProfileController::class, 'store'])->name('store');
+    });
 });
