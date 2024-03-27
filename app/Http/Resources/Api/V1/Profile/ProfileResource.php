799723747b0a0fd21a896919f3e6094dfd86ff2b
Jeremy ROBERT <contact@jeremyrobert.fr>
2024-03-27 16:24:25 +0100

    Add ProfileResource

---
+++ app/Http/Resources/Api/V1/Profile/ProfileResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Api\V1\Profile;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin \App\Models\Profile
+ */
+class ProfileResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'last_name' => $this->last_name,
+            'first_name' => $this->first_name,
+            'image' => $this->image,
+            'status' => $this->status,
+            'created_at' => $this->created_at->toDateTimeString(),
+        ];
+    }
+}
