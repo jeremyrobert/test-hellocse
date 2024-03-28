4e0e8e22f5e65a7ef1357698e5378aafdfe10ee8
Jeremy ROBERT <contact@jeremyrobert.fr>
2024-03-28 14:22:34 +0100

    Add CheckTokenAbilities middleware

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,15 +1,20 @@
 <?php
 
 use App\Http\Controllers\Api\V1\ProfileController;
+use App\Http\Middleware\CheckTokenAbilities;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->name('api.profiles.')->group(function () {
-    Route::post('profiles', [ProfileController::class, 'store'])->name('store');
-    Route::match(['put', 'patch', 'post'], 'profiles/{profile}', [ProfileController::class, 'update'])->where('profile', '[0-9]+')->name('update');
-    Route::delete('profiles/{profile}', [ProfileController::class, 'destroy'])->where('profile', '[0-9]+')->name('destroy');
-});
+Route::middleware(['auth:sanctum', CheckTokenAbilities::class])
+    ->name('api.profiles.')
+    ->group(function () {
+        Route::post('profiles', [ProfileController::class, 'store'])->name('store');
+        Route::match(['put', 'patch', 'post'], 'profiles/{profile}', [ProfileController::class, 'update'])->where('profile', '[0-9]+')->name('update');
+        Route::delete('profiles/{profile}', [ProfileController::class, 'destroy'])->where('profile', '[0-9]+')->name('destroy');
+    });
 
 // Dynamic middleware based on request header (to show the status of the profile or not => ProfileResource)
-Route::middleware(Request::header('Authorization') ? ['auth:sanctum'] : [])->name('api.profiles.')->group(function () {
-    Route::get('profiles', [ProfileController::class, 'index'])->where('profile', '[0-9]+')->name('index');
-});
+Route::middleware(Request::header('Authorization') ? ['auth:sanctum', CheckTokenAbilities::class] : [])
+    ->name('api.profiles.')
+    ->group(function () {
+        Route::get('profiles', [ProfileController::class, 'index'])->where('profile', '[0-9]+')->name('index');
+    });
