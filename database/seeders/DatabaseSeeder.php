20ac3ae22d0862ee3167683c6f23e0fffc4749f1
Jeremy ROBERT <contact@jeremyrobert.fr>
2024-03-28 15:23:18 +0100

    Add seed data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Administrator;
+use App\Models\Comment;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,9 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            AdministratorSeeder::class,
-            ProfileSeeder::class,
-        ]);
+        Administrator::factory()
+            ->hasProfiles(5)
+            ->count(3)
+            ->create()
+            ->each(function ($administrator) {
+                $administrator->profiles->each(function ($profile) use ($administrator) {
+                    Comment::factory()
+                        ->for($administrator)
+                        ->for($profile)
+                        ->create();
+                });
+            });
     }
 }
